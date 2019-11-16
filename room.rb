6396d9fe544f3cc15c9582e67cc8c2fda5f1e1fe
Grant Houston <grant.houston44@hotmail.com>
2019-11-16 12:43:28 +0000

    working check in/out function that considers the room capacity

--- room.rb
+++ room.rb
@@ -1,36 +1,48 @@
 require('pry')
 class Room
 
-attr_reader :room_num, :playlist, :entry_fee, :capacity
-attr_accessor :bill
+  attr_reader :room_num, :playlist, :entry_fee, :capacity, :guests
+  attr_accessor :bill
 
-def initialize(room_num, playlist)
-  @room_num = room_num
-  @playlist = playlist
-  @entry_fee = 10
-  @capacity = 10
-  @bill = 0
-  @guests = []
-end
+  def initialize(room_num, playlist)
+    @room_num = room_num
+    @playlist = playlist
+    @entry_fee = 10
+    @capacity = 10
+    @bill = 0
+    @guests = []
+  end
 
-def add_song(track)
-  @playlist.push(track)
-end
+  def add_song(track)
+    @playlist.push(track)
+  end
 
-def guest_count
-  @guests.size
-end
+  def guest_count
+    @guests.size
+  end
 
-def add_guest(person)
-  @guests.push(person)
-end
+  def add_guest(person)
+    @guests.push(person)
+  end
 
-def remove_guest(person)
-  @guests.delete(person)
-end
-# def thats_a_tune(room)
-#   @playlist.find {|playlist| playlist.title == room.playlist}
-#
-# end
+  def remove_guest(person)
+    @guests.delete(person)
+  end
+
+  def check_in(person)
+    if guest_count >= capacity
+      return p "Sorry, the room is full"
+    else
+      add_guest(person)
+    end
+  end
+
+  def check_out(person)
+    remove_guest(person)
+  end
+  # def thats_a_tune(room)
+  #   @playlist.find {|playlist| playlist.title == room.playlist}
+  #
+  # end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -4,6 +4,8 @@ require_relative('../room')
 require_relative('../guest')
 require_relative('../songs')
 
+
+
 MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 
 class TestRoom < MiniTest::Test
@@ -23,6 +25,15 @@ class TestRoom < MiniTest::Test
 
     @person1 = Guest.new("Grant", 50, "Shoot to thrill")
     @person2 = Guest.new("Ailsa", 50, "Ocean Avenue")
+    @person3 = Guest.new("Alan", 50, "Whole lotta Rosie")
+    @person4 = Guest.new("Chris", 50, "Beelzeboss")
+    @person5 = Guest.new("Cammy", 50, "Baggy Trousers")
+    @person6 = Guest.new("Scott", 50, "Feeling This")
+    @person7 = Guest.new("Steph", 50, "Rollercoaster")
+    @person8 = Guest.new("Alex", 50, "If your dad doesn't have a beard, you've got 2 mums")
+    @person9 = Guest.new("Jack", 50, "Got me a beard")
+    @person10 = Guest.new("Yasmin", 50, "Save it for the bedroom")
+    @person11 = Guest.new("Caithlyn", 50, "One Love")
 
 
     @room1 = Room.new(1, @playlist1)
@@ -63,19 +74,52 @@ class TestRoom < MiniTest::Test
     assert_equal(0, @room1.guest_count)
   end
 
-  def test_check_in_guest
+  def test_add_guest
     @room1.add_guest(@person1)
     assert_equal(1, @room1.guest_count)
   end
 
-  def test_check_out_guest
+  def test_remove_guest
     @room1.add_guest(@person1)
     @room1.add_guest(@person2)
     @room1.remove_guest(@person1)
-    binding.pry
     assert_equal(1, @room1.guest_count)
   end
 
+  def test_check_room_capacity
+    @room1.check_in(@person1)
+    @room1.check_in(@person2)
+    @room1.check_in(@person3)
+    @room1.check_in(@person4)
+    @room1.check_in(@person5)
+    @room1.check_in(@person6)
+    @room1.check_in(@person7)
+    @room1.check_in(@person8)
+    @room1.check_in(@person9)
+    @room1.check_in(@person10)
+    expected = @room1.check_in(@person11)
+    assert_equal(10, @room1.guest_count)
+    assert_equal("Sorry, the room is full", expected)
+  end
+
+  def test_check_out
+    @room1.check_in(@person1)
+    @room1.check_in(@person2)
+    @room1.check_in(@person3)
+    @room1.check_in(@person4)
+    @room1.check_in(@person5)
+    @room1.check_in(@person6)
+    @room1.check_in(@person7)
+    @room1.check_in(@person8)
+    @room1.check_in(@person9)
+    @room1.check_in(@person10)
+    @room1.check_out(@person1)
+    @room1.check_in(@person11)
+    assert_equal(10, @room1.guest_count)
+    assert_equal([@person2,@person3,@person4,@person5,@person6,@person7,@person8,@person9,@person10,@person11], @room1.guests)
+  end
+
+  
   # def test_can_get_fav_song
   #     expected = @person1.thats_a_tune(@room1)
   #   assert_equal("YAAAASSSS", expected)
